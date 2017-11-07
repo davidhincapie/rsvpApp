e614ba7952c7f055d4a3213f5123a70b9f489157
davidhincapie <davidohincapie@gmail.com>
2017-11-07 21:54:39 +0000

    refactored commit

--- app.js
+++ app.js
@@ -1,119 +1,117 @@
-const form = document.getElementById('registrar');
-const input = form.querySelector('input');
-const mainDiv = document.querySelector('.main');
-const ul = document.getElementById('invitedList');
-//creating the checkbox to see who hasn't responded to the rsvp
-const div = document.createElement('div');
-const filterLabel = document.createElement('label');
-const filterCheckBox = document.createElement('input');
-
-//checked box feature
-filterLabel.textContent = "Hide those who haven't responded";
-filterCheckBox.type = 'checkbox';
-div.appendChild(filterLabel);
-div.appendChild(filterCheckBox);
-mainDiv.insertBefore(div, ul);
-filterCheckBox.addEventListener('change', (e) =>{
-  const isChecked = e.target.checked;
-  const lis = ul.children;
-  if (isChecked) {
-    for (let i = 0; i < lis.length; i++) {
-      let li = lis[i];
-      if (li.className === 'responded') {
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('registrar');
+  const input = form.querySelector('input');
+  const mainDiv = document.querySelector('.main');
+  const ul = document.getElementById('invitedList');
+  //creating the checkbox to see who hasn't responded to the rsvp
+  const div = document.createElement('div');
+  const filterLabel = document.createElement('label');
+  const filterCheckBox = document.createElement('input');
+  
+  //checked box feature
+  filterLabel.textContent = "Hide those who haven't responded";
+  filterCheckBox.type = 'checkbox';
+  div.appendChild(filterLabel);
+  div.appendChild(filterCheckBox);
+  mainDiv.insertBefore(div, ul);
+  filterCheckBox.addEventListener('change', (e) =>{
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    if (isChecked) {
+      for (let i = 0; i < lis.length; i++) {
+        let li = lis[i];
+        if (li.className === 'responded') {
+          li.style.display = '';
+        } else {
+          li.style.display = 'none';
+        }
+      }
+    } else {
+      for (let i = 0; i < lis.length; i++) {
+        let li = lis[i];
         li.style.display = '';
-      } else {
-        li.style.display = 'none';
       }
     }
-  } else {
-    for (let i = 0; i < lis.length; i++) {
-      let li = lis[i];
-      li.style.display = '';
+  });
+  
+  //adding a guest in the page
+  function createLI(text) {
+    function createElement(elementName, property, value) {
+      const element = document.createElement(elementName);
+      element[property] = value;
+      return element;
     }
-  }
-});
-
-//adding a guest in the page
-function createLI(text) {
-  const li = document.createElement('li');
-  const span = document.createElement('span');
-  span.textContent = text;
-  li.appendChild(span);
-  const label = document.createElement('label');
-  label.textContent = 'Confirmed';
-  const checkbox= document.createElement('input');
-  checkbox.type = 'checkbox';
-  label.appendChild(checkbox);
-  li.appendChild(label);
-  
-  //adding an edit button
-  const editButton = document.createElement('button');
-  editButton.textContent = 'Edit';
-  li.appendChild(editButton);
-  
-  //adding a remove button
-  const removeButton = document.createElement('button');
-  removeButton.textContent = 'Remove';
-  li.appendChild(removeButton);
-  return li;
-}
-
-//adding a guest by writting in the input box
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const text = input.value;
-  input.value = '';
-  const li = createLI(text);
-  ul.appendChild(li);
-});
-
-//Checking the guests as confirmed
-ul.addEventListener('change', (e) => {
-  const checkbox = event.target;
-  const checked = checkbox.checked;
-  const listItem = checkbox.parentNode.parentNode;
-  
-  if (checked) {
-    listItem.className = 'responded';
-  } else {
-    listItem.className = '';
-  }
-});
-
-//removing guests with remove button
-ul.addEventListener('click', (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    const button = e.target;
-    const li = e.target.parentNode;
-    const ul = li.parentNode;
-    if (button.textContent === 'Remove') {
-      ul.removeChild(li);
-    } else if (button.textContent === 'Edit') {
-      const span = li.firstElementChild;
-      const input = document.createElement('input');
-      input.type = 'text';
-      input.value = span.textContent;
-      li.insertBefore(input, span);
-      li.removeChild(span);
-      button.textContent = 'Save';
-    } else if (button.textContent === 'Save') {
-      const input = li.firstElementChild;
-      const span = document.createElement('span');
-      span.textContent = input.value;
-      li.insertBefore(span, input);
-      li.removeChild(input);
-      button.textContent = 'Edit';
+    
+    function appendToLI(elementName, property, value) {
+      const element = createElement(elementName, property, value);
+      li.appendChild(element);
+      return element;
     }
+    
+    const li = document.createElement('li');
+    appendToLI('span', 'textContent', text);
+    appendToLI('label', 'textContent', 'Confirmed')
+      .appendChild(createElement('input', 'type', 'checkbox'));
+    //adding an edit button
+    appendToLI('button', 'textContent', 'edit');
+    //adding a remove button
+    appendToLI('button', 'textContent', 'remove');
+    return li;
   }
+  
+  //adding a guest by writting in the input box
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const li = createLI(text);
+    ul.appendChild(li);
+  });
+  
+  //Checking the guests as confirmed
+  ul.addEventListener('change', (e) => {
+    const checkbox = event.target;
+    const checked = checkbox.checked;
+    const listItem = checkbox.parentNode.parentNode;
+    
+    if (checked) {
+      listItem.className = 'responded';
+    } else {
+      listItem.className = '';
+    }
+  });
+  
+  //removing guests with remove button
+  ul.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      const button = e.target;
+      const li = e.target.parentNode;
+      const ul = li.parentNode;
+      const action = button.textContent;
+      const nameActions = {
+        remove: () => {
+          ul.removeChild(li);
+        },
+        edit: () => {
+          const span = li.firstElementChild;
+          const input = document.createElement('input');
+          input.type = 'text';
+          input.value = span.textContent;
+          li.insertBefore(input, span);
+          li.removeChild(span);
+          button.textContent = 'save';
+        }, 
+        save: () => {
+          const input = li.firstElementChild;
+          const span = document.createElement('span');
+          span.textContent = input.value;
+          li.insertBefore(span, input);
+          li.removeChild(input);
+          button.textContent = 'edit';
+        }
+      };
+      //select and run action in button's name
+      nameActions[action]();
+    }
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
